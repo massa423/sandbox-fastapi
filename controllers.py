6accf275eff9f9dfed661d1cc4425d77e0ca49db
Suzuki Masaya <vagrant@example.com>
2020-12-06 23:38:42 +0900

    サンプルToDoアプリ作成 その3
    see: https://rightcode.co.jp/blog/information-technology/fastapi-tutorial-todo-apps-authentication-user-registration

--- controllers.py
+++ controllers.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
-from starlette.requests import Request
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 from starlette.templating import Jinja2Templates, _TemplateResponse
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_405_METHOD_NOT_ALLOWED
 
 import db
 from models import User, Task
+
+import hashlib
+
+import re
+
+pattern = re.compile(r'\w{4,20}')
+pattern_pw = re.compile(r'\w{6,20}')
+pattern_mail = re.compile(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')
 
 app = FastAPI(
     title='FastAPIで作るToDoアプリケーション',
@@ -13,6 +24,8 @@ app = FastAPI(
 templates = Jinja2Templates(directory="templates")
 jinja_env = templates.env
 
+security = HTTPBasic()
+
 
 def index(request: Request) -> _TemplateResponse:
     """
@@ -21,16 +34,87 @@ def index(request: Request) -> _TemplateResponse:
     return templates.TemplateResponse('index.html', {'request': request})
 
 
-def admin(request: Request) -> _TemplateResponse:
+def admin(
+    request: Request, credentials: HTTPBasicCredentials = Depends(security)
+) -> _TemplateResponse:
     """
     admin
     """
-    user = db.session.query(User).filter(User.username == 'admin').first()
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    username = credentials.username
+    password = hashlib.sha256(credentials.password.encode()).hexdigest()
+
+    user = db.session.query(User).filter(User.username == username).first()
+    task = db.session.query(Task).filter(
+        Task.user_id == user.id).all() if user is not None else []
     db.session.close()
+
+    if user is None or user.password != password:
+        error = 'ユーザ名かパスワードが間違ってます'
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED,
+                            detail=error,
+                            headers={"WWW-Authentication": "Basic"})
 
     return templates.TemplateResponse('admin.html', {
         'request': request,
         'user': user,
         'task': task,
     })
+
+
+async def register(request: Request) -> _TemplateResponse:
+    """
+    register
+    """
+    if request.method == 'GET':
+        return templates.TemplateResponse('register.html', {
+            'request': request,
+            'username': '',
+            'error': []
+        })
+
+    if request.method == 'POST':
+        data = await request.form()
+        username = data.get('username')
+        password = data.get('password')
+        password_tmp = data.get('password_tmp')
+        mail = data.get('mail')
+
+        error = []
+
+        tmp_user = db.session.query(User).filter(
+            User.username == username).first()
+
+        if tmp_user is not None:
+            error.append('同じユーザ名が存在します。')
+
+        if password != password_tmp:
+            error.append('入力したパスワードが一致しません。')
+
+        if pattern.match(username) is None:
+            error.append('ユーザ名は4〜20文字の半角英数字にしてください。')
+
+        if pattern_pw.match(password) is None:
+            error.append('パスワードは6〜20文字の半角英数字にしてください。')
+
+        if pattern_mail.match(mail) is None:
+            error.append('正しくメールアドレスを入力してください。')
+
+        if error:
+            return templates.TemplateResponse('register.html', {
+                'request': request,
+                'username': username,
+                'error': error
+            })
+
+        user = User(username, password, mail)
+        db.session.add(user)
+        db.session.commit()
+        db.session.close()
+
+        return templates.TemplateResponse('complete.html', {
+            'request': request,
+            'username': username
+        })
+
+    msg = '許可されていません。'
+    raise HTTPException(status_code=HTTP_405_METHOD_NOT_ALLOWED, detail=msg)
--- create_table.py
+++ create_table.py
@@ -1,6 +1,5 @@
 from models import SQLITE3_NAME, User, Task
-import db
-from db import Base
+from db import Base, session, engine
 
 import os
 from datetime import datetime
@@ -8,13 +7,13 @@ from datetime import datetime
 if __name__ == "__main__":
     path = SQLITE3_NAME
     if not os.path.isfile(path):
-        Base.metadata.create_all(db.engine)
+        Base.metadata.create_all(engine)
 
     admin = User(username='admin',
                  password='fastapi',
                  mail='admin@example.com')
-    db.session.add(admin)
-    db.session.commit()
+    session.add(admin)
+    session.commit()
 
     task = Task(
         user_id=admin.id,
@@ -22,7 +21,7 @@ if __name__ == "__main__":
         deadline=datetime(2020, 12, 6, 12, 0, 0),
     )
     print(task)
-    db.session.add(task)
-    db.session.commit()
+    session.add(task)
+    session.commit()
 
-    db.session.close()
+    session.close()
